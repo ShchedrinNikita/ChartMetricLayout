429a78bde8f223113e0a6d43d4cdd7054121d4b4
Your Name <shedrinnikita@gmail.com>
2019-06-01 14:20:55 +0300

    added white header

--- src/components/modules/burger-menu/burgerMenu.js
+++ src/components/modules/burger-menu/burgerMenu.js
@@ -2,6 +2,7 @@ function initBurger(selector, $) {
     $(selector).find('> .hamburger').click(function(){
         $(this).toggleClass('is-active')
         $(this).siblings('.hidden-part').slideToggle()
+        $(this).parents('.header_cn').toggleClass('white')
     })
 }
 export default initBurger
